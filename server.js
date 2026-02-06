cd48a4b37e75f61cd6885f5ede5ececabe4d66bb
gdx-u <142684990+gdx-u@users.noreply.github.com>
2026-02-06 13:03:47 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { spawn } = require("child_process");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+const jPath = path.join(__dirname, "j901/bin/jconsole");
+
+app.post("/eval", (req, res) => {
+  const code = req.body.code ?? "";
+
+  const j = spawn(jPath, [], { stdio: "pipe" });
+
+  let output = "";
+
+  j.stdout.on("data", (d) => (output += d.toString()));
+  j.stderr.on("data", (d) => (output += d.toString()));
+
+  // Prevent hanging
+  const kill = setTimeout(() => j.kill("SIGKILL"), 2000);
+
+  j.stdin.write(code + "\nexit 0\n");
+  j.stdin.end();
+
+  j.on("close", () => {
+    clearTimeout(kill);
+    res.send({ output });
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`J backend running on port ${port}`));
