70a9e8d69484d38ca633fe8dc7d2d2373a90b2f7
gdx-u <142684990+gdx-u@users.noreply.github.com>
2026-02-06 13:21:44 +0000

    Add timeout

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 const express = require("express");
 const { spawn } = require("child_process");
 const path = require("path");
-const jPath = path.join(__dirname, "j9.6/bin/jconsole");
 const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const j_path = path.join(__dirname, "j9.6/bin/jconsole");
+
 app.post("/eval", (req, res) => {
-  const code = req.body.code ?? "";
+    const code = req.body.code ?? "";
 
-  const j = spawn(jPath, [], { stdio: "pipe" });
+    const j_process = spawn(j_path, [], { stdio: "pipe" });
+    let output = "";
 
-  let output = "";
+    j_process.stdout.on("data", (data) => output += data.toString());
+    j_process.stderr.on("data", (data) => output += data.toString());
 
-  j.stdout.on("data", (d) => (output += d.toString()));
-  j.stderr.on("data", (d) => (output += d.toString()));
+    // Kill the process if it runs longer than 30 seconds
+    const kill_timeout = setTimeout(() => {
+        j_process.kill("SIGKILL");
+        output += "\n[Error: Execution timed out after 30 seconds]";
+        res.send({ output });
+    }, 30000);
 
-  // Prevent hanging
-  const kill = setTimeout(() => j.kill("SIGKILL"), 2000);
+    j_process.stdin.write(code + "\nexit 0\n");
+    j_process.stdin.end();
 
-  j.stdin.write(code + "\nexit 0\n");
-  j.stdin.end();
-
-  j.on("close", () => {
-    clearTimeout(kill);
-    res.send({ output });
-  });
+    j_process.on("close", () => {
+        clearTimeout(kill_timeout);
+        res.send({ output });
+    });
 });
 
 const port = process.env.PORT || 3000;
